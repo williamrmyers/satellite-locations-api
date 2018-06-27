efc4384814fcffa8db1a8457d81a6d9b508e0bfa
William Myers <williamrmyers@gmail.com>
2018-06-26 20:49:07 -0700

    Added Pagination, and /satellite/purposes  route.

--- server/server.js
+++ server/server.js
@@ -10,19 +10,58 @@ const {Satellite} = require('./models/satellite')
 
 const app = express();
 const port = process.env.PORT;
-
 app.use(bodyParser.json());
 
-app.get('/all/geo', async (req, res) => {
-  try {
-    const satelites = await Satellite.find({ apogee: { $exists: true, $nin: null } }, 'names apogee perigee period eccentricity inclination period');
+const maxNumberOfSatellites = 1738;
 
-    if (!satelites) {
+app.get('/all/geo/', async (req, res) => {
+  const limit = _.toNumber(req.query.limit);
+  const start = _.toNumber(req.query.start);
+  // Returns the start object if less than the max otherwise returns null
+  const startCursor = (limit, start, max) => max >= limit + start? {"limit": limit + start} : null;
+  try {
+    const satellites = await Satellite.find({}, 'names apogee perigee period eccentricity inclination period')
+    .skip(start)
+    .limit(limit);
+
+    if (!satellites) {
       return res.status(404).send({});
     }
 
-    res.send({satelites});
+    res.send({satellites,limit, start: startCursor(limit, start, maxNumberOfSatellites)});
 
+  } catch (e) {
+    return res.status(404).send({});
+  }
+});
+
+app.get('/all/geo', async (req, res) => {
+  try {
+    const satellites = await Satellite.find({}, 'names apogee perigee period eccentricity inclination period');
+
+    if (!satellites) {
+      return res.status(404).send({});
+    }
+
+    res.send({});
+
+  } catch (e) {
+    return res.status(404).send({});
+  }
+});
+
+// get list of satelite purposes
+app.get('/satellite/purposes', async (req, res) => {
+  try {
+    const satellites = await Satellite.aggregate(
+      [
+        { $group : { _id : "$purpose", total : { $sum : 1 } } }
+      ]
+    );
+    if (!satellites) {
+      return res.status(404).send({});
+    }
+    res.send({satellites});
   } catch (e) {
     return res.status(404).send({});
   }
@@ -34,10 +73,6 @@ app.listen(port, ()=>{
 
 //Future Routes
 
-// get list of satelite purposes
-
-
-// Return a list of satelites for one specific purposes
-
+// Return a list of satellites for one specific purposes
 
 // Get All data for a Sateltite by ID.
